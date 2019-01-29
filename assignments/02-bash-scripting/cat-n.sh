a51a857455cacfec52f9bb2be747eb5773f4be15
Shelby Nelson <shelbeezy@email.arizona.edu>
2019-01-28 19:28:49 -0700

    homework 2

---
+++ assignments/02-bash-scripting/cat-n.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+FILE=${1:-'sonnet-29.txt'}
+
+if [[ $# -eq 0 ]]; then
+	echo "Usage: cat-n.sh FILE"
+	exit 1
+fi
+
+if [[ -f "$ARG" ]]; then
+	echo "$ARG is not a file"
+fi
+
+i=0
+while read -r LINE; do
+	i=$((i+1))
+	echo $i "$LINE"
+done < "$FILE"

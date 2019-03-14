7ff65a085905cca91c7bc839add7a6d948389d75
Shelby Nelson <shelbeezy@email.arizona.edu>
2019-03-13 20:38:46 -0700

    HW7unfinished

---
+++ assignments/07-csv/blastomatic.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""
+Author : shelbeezy
+Date   : 2019-03-13
+Purpose: Parsing a CSV file
+"""
+
+import csv
+import argparse
+import sys
+import os
+from collections import defaultdict
+
+# --------------------------------------------------
+def get_args():
+    """get command-line arguments"""
+    parser = argparse.ArgumentParser(
+        description='Annotate BLAST output',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        'mainfile', metavar='FILE', help='BLAST output (-outfmt 6)')
+
+    parser.add_argument(
+        '-a',
+        '--annotations',
+        help='Annotation file',
+        metavar='FILE',
+        type=str,
+        default='')
+
+    parser.add_argument(
+        '-o',
+        '--outfile',
+        help='Output file',
+        metavar='FILE',
+        type=str,
+        default='')
+
+    
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def warn(msg):
+    """Print a message to STDERR"""
+    print(msg, file=sys.stderr)
+
+
+# --------------------------------------------------
+def die(msg='Something bad happened'):
+    """warn() and exit with error"""
+    warn(msg)
+    sys.exit(1)
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+    args = get_args()
+    main_file = args.mainfile
+    anno_file = args.annotations
+    out_file = args.outfile
+  
+    if not os.path.isfile(main_file):
+        die('"{}" is not a file'.format(main_file))
+    if not os.path.isfile(anno_file):
+        die('"{}" is not a file'.format(anno_file))
+   
+    with open(anno_file) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
+    
+    with open(main_file) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',', fieldnames=('qaccver', 'saccver', 'piednt', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore'))
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
